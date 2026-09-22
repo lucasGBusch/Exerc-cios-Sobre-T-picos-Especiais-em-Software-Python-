a681ff97355470d46abbb53feef00513dd03e1c6
lucasGBusch <lucasbusch493@gmail.com>
2026-09-22 17:37:09 -0300

    ex19 desempacotamento de tupla

---
+++ Exercicios Basicos Python/ex19.py
@@ -0,0 +1,14 @@
+"""
+
+2. Crie uma tupla com nome, idade e curso de um aluno. Utilize
+desempacotamento para armazenar os valores em três variáveis.
+
+"""
+
+aluno = ("nome\n", "idade\n", "curso")
+
+nome, idade, curso = aluno
+
+print(nome)
+print(idade)
+print(curso)
