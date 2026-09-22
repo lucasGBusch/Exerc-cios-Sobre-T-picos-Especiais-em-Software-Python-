6671c5e0b337a2e50dedc6dcadafc756c7757545
lucasGBusch <lucasbusch493@gmail.com>
2026-09-22 17:32:17 -0300

    ex18 tupla

---
+++ Exercicios Basicos Python/ex18.py
@@ -0,0 +1,12 @@
+"""
+
+1. Crie uma tupla com cinco cidades. Mostre todas as cidades e
+depois exiba a primeira e a última.
+
+"""
+
+cidade = ("Curitiba", "São Paulo", "Londrina", "Maringa", "Castro")
+
+print(cidade)
+print(cidade[0])
+print(cidade[4])
